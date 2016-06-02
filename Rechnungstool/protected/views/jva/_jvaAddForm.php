c8b0bf759f89f29465da68eb8179f97de7dbac29
Beni <Benni.lempi@gmail.com>
2016-06-02 14:08:17 +0200

    add

--- Rechnungstool/protected/views/jva/_jvaAddForm.php
+++ Rechnungstool/protected/views/jva/_jvaAddForm.php
@@ -40,7 +40,7 @@ if(isset($jvaAddFormModel)){
 			//var_dump($colNames);			
 			//Awesome C like fix
 			foreach($tabs as &$tab) {
-				for($i=1;$i<7;$i++){
+				for($i=1;$i<6;$i++){
 					if($tab['label'] == 'IK'){
 						$idTab = "-Ik";
 					}else if($tab['label'] == 'Logistik Memmelsdorf'){
@@ -54,7 +54,7 @@ if(isset($jvaAddFormModel)){
 					}
 					//ADD PRE-DEFINED COLUMNS
 					$tab['content'] .= $form->dropDownListGroup($jvaAddFormModel,'colName'.$i, array('widgetOptions'=>array('data'=>$colNames,'htmlOptions' => array('id'=>   'addColName'.$i .$idTab)),'class'=>'col-sm-5', 'label' => 'Spalte '.$i));
-					if($i == 6){
+					if($i == 5){
 						$tab['content'] .= $form->textFieldGroup($jvaAddFormModel,'printAmount',array('widgetOptions'=>array('htmlOptions' => array('id'=>   'printAmount'.$idTab)), 'label'=>'Anzahl Ausdrucke'));
 					}
 				}
